b7223cfd673724dd9332e5ef5c96b1d387f7b8d3
Matt <therealtalslain@gmail.com>
2020-04-13 00:12:32 +0100

    Updates

---
+++ precache-manifest.44455902181c8ed9efb387bbedd8a231.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b19b57b940a316e6f34f7f777f30dd7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "447bd133fd7bf71bed87",
+    "url": "/monsters-rolodex/static/css/main.dbb17956.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "447bd133fd7bf71bed87",
+    "url": "/monsters-rolodex/static/js/main.5ff59ea0.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
